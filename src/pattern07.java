b56a3b0680383ef2a58379e3ac934ddff59d2a5f
FahimFBA <fahimbinamin@gmail.com>
2022-03-17 15:28:56 +0600

    solved pattern 7

---
+++ src/pattern07.java
@@ -0,0 +1,24 @@
+public class pattern07 {
+    public static void main(String[] args) {
+        pattern07(5);
+    }
+
+    static void pattern07(int n) {
+        for (int row = 1; row <= n; row++) {
+
+            for (int space = 0; space < n - row; space++) {
+                System.out.print("  ");
+            }
+
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
